ba3e573ceadbe1bf9f857c1448b83e769be4275c
BackupMiles <leanwithmiles@gmail.com>
2024-12-01 18:48:32 +0100

    feat: init

---
+++ build/CMakeFiles/day-01.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/miles/Desktop/dev/aoc-2024/day-01.cpp" "CMakeFiles/day-01.dir/day-01.cpp.o" "gcc" "CMakeFiles/day-01.dir/day-01.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
