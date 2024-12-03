de7618a45a5ec4547988c31e602533a0e10cc561
BackupMiles <leanwithmiles@gmail.com>
2024-12-03 19:12:42 +0100

    feat: add part 2

--- day-03/day-03.cpp
+++ day-03/day-03.cpp
@@ -8,6 +8,7 @@
  * add up all the results
  */
 
+std::string clean_line(const std::string& line, const std::regex& dont_regex);
 int sum_line_multiplications(const std::string& line, const std::regex& mul_regex);
 
 int main() {
@@ -22,14 +23,33 @@ int main() {
     // input is still divided in lines, time for a good ol stream
     std::string line;
     int sum = 0;
+    std::regex dont_regex(R"rgx((don\'t\(\).*?)(?=do\(\)|$))rgx");
     std::regex mul_regex(R"rgx(mul\(([0-9]{1,3})\,([0-9]{1,3})\))rgx");
 
     while (std::getline(inputFile, line)) {
-        sum += sum_line_multiplications(line, mul_regex);
+        std::string clean = (clean_line(line, dont_regex));
+        sum += sum_line_multiplications(clean, mul_regex);
     }
 
     inputFile.close();
     std::cout << sum;
+}
+
+// remove all the don't-s sequences from the input string
+std::string clean_line(const std::string& line, const std::regex& dont_regex) {
+    auto mul_begin = std::regex_iterator(line.begin(), line.end(), dont_regex);
+    auto mul_end = std::sregex_iterator();
+
+    std::string result(line);
+
+    for (std::regex_iterator i = mul_begin; i != mul_end; i++) {
+        std::smatch match = *i;
+        std::string as_str = match.str();
+
+        result = std::regex_replace(result, dont_regex, "");
+    }
+
+    return result;
 }
 
 int sum_line_multiplications(const std::string& line, const std::regex& mul_regex) {
